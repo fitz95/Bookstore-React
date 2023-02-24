94ac7a3fe29871db5688602c4a2cda0deea969b0
fitz95 <fitzgeraldnsong@gmail.com>
2023-02-24 17:51:42 +0100

    remove extra button

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -6,17 +6,11 @@ function AddForm() {
     <>
       <form className="form-container">
         <input type="text" placeholder="Add Author" className="input-text" />
-        <button className="input-submit" type="button">
-          <FcPlus
-            style={{
-              fontSize: '20px',
-              marginTop: '2px',
-            }}
-          />
-        </button>
         <br />
         <br />
         <input type="text" placeholder="Add Title" className="input-text" />
+        <br />
+        <br />
         <button className="input-submit" type="button">
           <FcPlus
             style={{
